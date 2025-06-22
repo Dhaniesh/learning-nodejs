f225c97579bbc750eb6446f2eaea74a526792c1a
Dhaniesh Murugan <dhaniesh.m@idp.com>
2025-06-22 13:33:44 +0530

    add notes router and controller

---
+++ backend/src/controllers/notesController.js
@@ -0,0 +1,15 @@
+export function getAllNotes(req, res) {
+    res.json({ "message": "here is your note" })
+}
+
+export function createNote(req, res) {
+    res.json({ "message": "here is your note" })
+}
+
+export function updateNote(req, res) {
+    res.json({ "message": "here is your note" })
+}
+
+export function deleteNote(req, res) {
+    res.json({ "message": "here is your note" })
+}
