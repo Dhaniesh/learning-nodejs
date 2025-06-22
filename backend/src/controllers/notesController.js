505422d6ba113d97596290f67196a5183b6638c7
Dhaniesh Murugan <dhaniesh.m@idp.com>
2025-06-22 16:13:03 +0530

    controller to implement async functions for getting and creating notes

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,9 +1,27 @@
-export function getAllNotes(req, res) {
-    res.json({ "message": "here is your note" })
+import Note from "../models/Note.js"
+
+export async function getAllNotes(req, res) {
+    try {
+        const notes = await Note.find();
+        res.status(200).json(notes)
+    } catch (error) {
+        res.status(500).json({ "message": "Internal server error" })
+    }
 }
 
-export function createNote(req, res) {
-    res.json({ "message": "here is your note" })
+export async function createNote(req, res) {
+    try {
+        const { title, content } = req.body
+        const newNote = new Note({ title, content })
+
+        await newNote.save();
+        res.status(201).json({"message": "Note created successully"})
+    } catch (error) {
+        console.log("Error in creating note");
+        res.status(500).json({ "message": "Internal server error" })
+        
+    }
+
 }
 
 export function updateNote(req, res) {
